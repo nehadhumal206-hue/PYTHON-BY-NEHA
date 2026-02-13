17e2d38507a443967f7c4b2fff45e32d22badf64
nehadhumal206-hue <nehadhumal206@gmail.com>
2026-02-13 13:33:52 +0530

    Add files via upload

---
+++ EX2B.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 13 07:36:18 2026
+
+@author: User
+"""
+
+n=int(input("Enter number:"))
+fact=1
+for i in range(1,n+1):
+    fact=fact*1
+    print("Factorial:",fact)
